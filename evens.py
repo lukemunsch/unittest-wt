bee1fce7dab3b0070c5ddc64b26880ad503c87b9
lukemunsch <lpmunsch@gmail.com>
2022-01-10 23:39:23 +0000

    add content changes to evens and test files

--- evens.py
+++ evens.py
@@ -9,15 +9,21 @@ def even_number_of_evens(numbers):
     """
 
     if isinstance(numbers, list):
-        if numbers == []:
-            return False
+        evens = 0
+
+        print(n for n in numbers)
+
+        for n in numbers:
+            if n % 2 == 0:
+                evens += 1
+
+        if evens:
+            return evens % 2 == 0
         else:
-            return True    
+            return False
     else:
         raise TypeError("A list was not passed into tehe function!")
 
-    
-    
 
 if __name__ == '__main__':
-    print(even_number_of_evens(5))
+    even_number_of_evens([2, 1, 4])
